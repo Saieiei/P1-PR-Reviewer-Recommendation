f8c4254b32c1314e59ed5228c052ed3b5530a011
Sairudra More <sairudra60@gmail.com>
2025-04-08 22:05:23 +0530

    Update PRE_RUN.bash
    Fixed the order

--- PRE_RUN.bash
+++ PRE_RUN.bash
@@ -2,9 +2,9 @@
 module purge
 module load cray-python/3.11.7
 module load PrgEnv-nvidia/8.6.0
-module load nvidia/20.11
+module swap nvidia/20.11 nvidia/24.7
 pip install --upgrade pip
 pip install requests
 pip install -r requirements2.txt
 pip install torch torchvision --extra-index-url https://download.pytorch.org/whl/cu116
-python offloading-&&-distributed_script.py
+python offloading_distributed_script.py
